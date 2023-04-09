8b507a4e5fd11f3176dd840fe7e66feae7927b1e
untrusting <128155219+untrusting@users.noreply.github.com>
2023-04-09 13:38:38 +0100

    Increase the z-index of the navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </style>
 
 <main class="flex flex-col max-w-[1280px] m-auto px-6">
-    <nav class="fixed top-0 left-0 right-0 mx-auto z-[2] backdrop-blur text-white py-1 border-b-[1px] border-gray-700">
+    <nav class="fixed top-0 left-0 right-0 mx-auto z-[10] backdrop-blur text-white py-1 border-b-[1px] border-gray-700">
         <div class="flex text-md px-2 space-x-6 w-full max-w-[1280px] mx-auto">
             <a class="flex flex-col font-bold text-4xl my-auto" href="/">
                 <h1 class="tracking-widest p-2">BNC</h1>
