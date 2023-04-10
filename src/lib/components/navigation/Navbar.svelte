fb74411d8f34e58fa47bf7323e52439afe88b29c
untrusting <128155219+untrusting@users.noreply.github.com>
2023-04-10 16:35:24 +0100

    Close mobile navigation when window is resized

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -10,6 +10,8 @@
     }
 </script>
 
+<svelte:window on:resize={() => open = false} />
+
 <nav class="fixed top-0 left-0 right-0 mx-auto z-[12] backdrop-blur text-white py-1 border-b-[1px] border-gray-700" in:fade={{ delay: 200, duration: 300 }}>
     <div class="flex text-md px-2 space-x-6 w-full max-w-[1280px] mx-auto">
         <a class="flex flex-col font-bold text-4xl my-auto" href="/">
