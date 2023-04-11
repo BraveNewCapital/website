22ed3d0c41f74c70740221755cd78720a86ff5d8
untrusting <128155219+untrusting@users.noreply.github.com>
2023-04-11 12:58:01 +0100

    Lock body scrolling when mobile menu is open

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -1,18 +1,22 @@
 <script>
     import { Hamburger } from 'svelte-hamburgers';
 	import MobileNav from './MobileNav.svelte';
-	import { fade, slide } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
+    import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
     let open;
+    let navbar;
 
     let toggle = () => {
         open = !open;
     }
+
+    $: open, navbar ? open ? disableBodyScroll(navbar) : enableBodyScroll(navbar) : null;
 </script>
 
 <svelte:window on:resize={() => open = false} />
 
-<nav class="fixed top-0 left-0 right-0 mx-auto z-[12] backdrop-blur text-white py-1 border-b-[1px] border-gray-700" in:fade={{ delay: 200, duration: 300 }}>
+<nav bind:this={navbar} class="fixed top-0 left-0 right-0 mx-auto z-[12] backdrop-blur text-white py-1 border-b-[1px] border-gray-700" in:fade={{ delay: 200, duration: 300 }}>
     <div class="flex text-md px-2 space-x-6 w-full max-w-[1280px] mx-auto">
         <a class="flex flex-col font-bold text-4xl my-auto" href="/">
             <h2 class="tracking-widest p-2">BNC</h2>
