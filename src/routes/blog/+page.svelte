f36278eda2f3be12718290c7407c92705c7000da
untrusting <128155219+untrusting@users.noreply.github.com>
2023-04-14 16:11:36 +0100

    Blog styling and date formatting

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,15 +1,17 @@
 <script>
     export let data;
+
+    import date from 'date-and-time';
 </script>
 
 <div class="w-full text-white mt-32 w-full">
     <div class="mt-8 flex flex-col w-full max-w-[1024px] mx-auto space-y-12">
         {#each data.posts as post}
-            <a href="{post.path}" class="flex flex-col bg-gray-700/30 rounded-lg p-8 md:p-12">
-                <h2 class="monument-extended text-3xl xs:text-5xl md:text-7xl line-clamp-2">{post.meta.title}</h2>
-                <p class="max-h-[140px] text-lg my-6 line-clamp-3">{post.meta.brief}</p>
-                <img class="rounded-lg h-[300px] object-cover h-[300px] w-full" src="{post.meta.image}" alt="{post.meta.title}" />
-                <p class="text-gray-500 text-sm mt-6 ml-auto">{post.meta.date}</p>
+            <a href="{post.path}" class="flex flex-col bg-gray-700/30 hover:bg-gray-700/40 rounded-lg p-8 md:p-16">
+                <h2 class="monument-extended text-3xl xs:text-5xl md:text-7xl">{post.meta.title}</h2>
+                <p class="max-h-[140px] text-xl my-6 line-clamp-2 leading-9">{post.meta.brief}</p>
+                <img class="rounded-lg h-[370px] object-cover w-full" src="{post.meta.image}" alt="{post.meta.title}" />
+                <p class="text-gray-500 text-sm mt-6 ml-auto">{date.format(date.parse(post.meta.date, 'YYYY-MM-DD'), 'ddd, MMM DD YYYY')}</p>
             </a>
         {/each}
     </div>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     export let data;
+
+    import date from 'date-and-time';
 </script>
 
 <svelte:head>
@@ -10,7 +12,7 @@
 <article class="text-white mt-32">
     <h1 class="monument-extended text-5xl md:text-7xl">{data.title}</h1>
     <img class="rounded-lg h-[300px] object-cover w-full my-4" src="{data.image}" alt="{data.title}" />
-    <p class="text-gray-500 text-sm">Published: {data.date}</p>
+    <p class="text-gray-500 text-sm">{date.format(date.parse(data.date, 'YYYY-MM-DD'), 'ddd, MMM DD YYYY')}</p>
     <p class="text-gray-500 text-sm">by {data.author}</p>
 
     <div class="content my-8">
@@ -27,7 +29,7 @@
                 <div class="flex flex-col w-full md:px-4 h-[186px] mt-4 md:mt-0">
                     <h2 class="monument-extended text-4xl line-clamp-1">{article.meta.title}</h2>
                     <p class="line-clamp-3 my-4 text-lg leading-8">{article.meta.brief}</p>
-                    <p class="text-sm text-gray-500">{article.meta.date}</p>
+                    <p class="text-sm text-gray-500">{date.format(date.parse(article.meta.date, 'YYYY-MM-DD'), 'ddd, MMM DD YYYY')}</p>
                 </div>
             </a>
         {/each}
