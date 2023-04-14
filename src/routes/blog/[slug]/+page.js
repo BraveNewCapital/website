37eaacfc5cc500077277dbe34545359d5c7f29cb
untrusting <128155219+untrusting@users.noreply.github.com>
2023-04-14 13:30:14 +0100

    Added a "read more" section for each article

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,14 +1,19 @@
-// src/routes/blog/[slug]/+page.js
-export async function load({ params }){
+export async function load({ params, fetch }){
     const post = await import(`../posts/${params.slug}.md`);
     const { title, date, author, image } = post.metadata;
     const content = post.default;
-  
+
+    const response = await fetch(`/api/posts`);
+    const posts = await response.json();
+
+    const related = posts.filter(post => post && post.meta.title !== title).slice(0, 3);
+
     return {
         content,
         title,
         date,
         author,
-        image
+        image,
+        related
     }
 }
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,4 +1,3 @@
-<!-- src/routes/[slug]/+page.svelte -->
 <script>
     export let data;
 </script>
@@ -10,7 +9,7 @@
 
 <article class="text-white mt-32">
     <h1 class="monument-extended text-5xl md:text-7xl">{data.title}</h1>
-    <img class="rounded-lg h-[300px] object-cover h-[300px] w-full my-4" src="{data.image}" alt="{data.title}" />
+    <img class="rounded-lg h-[300px] object-cover w-full my-4" src="{data.image}" alt="{data.title}" />
     <p class="text-gray-500 text-sm">Published: {data.date}</p>
     <p class="text-gray-500 text-sm">by {data.author}</p>
 
@@ -18,3 +17,19 @@
         <svelte:component this={data.content} />
     </div>
 </article>
+
+<section class="text-white my-8">
+    <h1 class="monument-extended text-4xl md:text-5xl mb-8">Read More</h1>
+    <div class="space-y-8">
+        {#each data.related as article}
+            <a class="flex flex-col md:flex-row" href="{article.path}">
+                <img class="rounded-lg w-full md:w-[248px] h-[248px] md:h-[186px] object-cover" src="{article.meta.image}" alt="{article.meta.title}" />
+                <div class="flex flex-col w-full md:px-4 h-[186px] mt-4 md:mt-0">
+                    <h2 class="monument-extended text-4xl line-clamp-1">{article.meta.title}</h2>
+                    <p class="line-clamp-3 my-4 text-lg leading-8">{article.meta.brief}</p>
+                    <p class="text-sm text-gray-500">{article.meta.date}</p>
+                </div>
+            </a>
+        {/each}
+    </div>
+</section>
