5f08d7be052c3345281b61c50e6f79745d38a293
untrusting <128155219+untrusting@users.noreply.github.com>
2023-04-14 15:54:09 +0100

    Some styling for the navbar

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -16,16 +16,22 @@
 
 <svelte:window on:resize={() => open = false} />
 
-<nav bind:this={navbar} class="fixed top-0 left-0 right-0 mx-auto z-[12] backdrop-blur text-white py-1 border-b-[1px] border-gray-700" in:fade={{ delay: 200, duration: 300 }}>
+<nav bind:this={navbar} class="fixed top-0 left-0 right-0 mx-auto z-[12] backdrop-blur text-white border-b-[1px] border-gray-700" in:fade={{ delay: 200, duration: 300 }}>
     <div class="flex text-md px-2 space-x-6 w-full max-w-[1280px] mx-auto">
         <a class="flex flex-col font-bold text-4xl my-auto" href="/">
             <h2 class="tracking-widest p-2 nosignal">BNC</h2>
         </a>
         <div class="my-auto w-full">
-            <div class="hidden sm:block space-x-6 ml-8">
-                <a class="underline-offset-8 decoration-2 hover:underline" href="/">Projects</a>
-                <a class="underline-offset-8 decoration-2 hover:underline" href="/blog">Blog</a>
-                <a class="underline-offset-8 decoration-2 hover:underline" href="/">Mission</a>
+            <div class="hidden sm:block space-x-6 ml-8 h-[60px]">
+                <a class="underline-offset-8 decoration-2 hover:underline" href="/">
+                    <button class="h-full border-t-2 border-transparent hover:border-[#03ffff] w-[80px] transition ease-in-out duration-400">Projects</button>
+                </a>
+                <a class="underline-offset-8 decoration-2 hover:underline" href="/blog">
+                    <button class="h-full border-t-2 border-transparent hover:border-[#03ffff] w-[80px] transition ease-in-out duration-400">Blog</button>
+                </a>
+                <a class="underline-offset-8 decoration-2 hover:underline" href="/">
+                    <button class="h-full border-t-2 border-transparent hover:border-[#03ffff] w-[80px] transition ease-in-out duration-400">Mission</button>
+                </a>
             </div>
         </div>
         <div class="flex">
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,7 +6,7 @@
     <div class="mt-8 flex flex-col w-full max-w-[1024px] mx-auto space-y-12">
         {#each data.posts as post}
             <a href="{post.path}" class="flex flex-col bg-gray-700/30 rounded-lg p-8 md:p-12">
-                <h2 class="monument-extended text-3xl xs:text-5xl md:text-8xl">{post.meta.title}</h2>
+                <h2 class="monument-extended text-3xl xs:text-5xl md:text-7xl line-clamp-2">{post.meta.title}</h2>
                 <p class="max-h-[140px] text-lg my-6 line-clamp-3">{post.meta.brief}</p>
                 <img class="rounded-lg h-[300px] object-cover h-[300px] w-full" src="{post.meta.image}" alt="{post.meta.title}" />
                 <p class="text-gray-500 text-sm mt-6 ml-auto">{post.meta.date}</p>
