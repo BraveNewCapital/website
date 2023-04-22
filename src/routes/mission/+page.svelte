bacab30ac96d00c953bc97d075cb51387d62391d
untrusting <128155219+untrusting@users.noreply.github.com>
2023-04-22 13:02:44 +0100

    Mission text formatting

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <section class="w-full max-w-[800px] mx-auto text-white mt-32 text-xl space-y-6 leading-9" in:fade>
-    <p class="italic">Shaping Web3 through Cryptography, Self-Sovereignty, and Incentivised Trust</p>
+    <h1 class="text-3xl md:text-5xl leading-12 font-bold">Shaping Web3 through Cryptography, Self-Sovereignty, and Incentivised Trust</h1>
 
-    <p>At BraveNewCapital, we recognise that the advent of Bitcoin marked the beginning of a transformative era, paving the way for Web3 – a decentralised digital landscape that unites the core technologies needed to build systems that reflect our guiding principles. Our mission is to harness the power of Web3 to create innovative projects that embody the analogous relationship between cryptography, trustlessness, and self-sovereignty, empowering individuals to take control of their own identity, reputation, and data.</p>
+    <p>At BraveNewCapital, we recognise that the advent of Bitcoin marked the beginning of a transformative era, paving the way for Web3 – a decentralised digital landscape that unites the core technologies needed to build systems that reflect our guiding principles.</p>
+        
+    <p>Our mission is to harness the power of Web3 to create innovative projects that embody the analogous relationship between cryptography, trustlessness, and self-sovereignty, empowering individuals to take control of their own identity, reputation, and data.</p>
 
     <p>Cryptography fuels trustlessness, eliminating the need for reliance on centralised authorities and enabling self-sovereignty in the digital realm. By employing cryptographic principles, we develop systems that place the ownership and control of one's digital identity and data firmly in the hands of individuals.</p>
 
